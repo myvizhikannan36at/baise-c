5fb45873fec9bbf0cdb4bbbc8cf7a56b7ab0ea61
Your Name <your-email@example.com>
2025-12-22 22:53:10 +0530

    Added programs 11 to 15

---
+++ program 12.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    char college[] ="SSCET";
+    printf("My college is %s",college);
+    return 0;
+}
---
+++ program 13.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    float num;
+    scanf("%f",&num);
+    printf("decimal number %.2f \n",num);
+    return 0;
+}
