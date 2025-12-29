dea0c2235557e4f87d5d81a3aec4099056a97906
myvizhikannan36at <myk.offical.07@gmail.com>
2025-12-29 23:20:03 +0530

    Add files via upload

---
+++ program 23.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    int x=45;
+    printf("%d",--x);
+    
+    return 0;
+}
---
+++ program 25.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int x = 23;
+    ++x;
+    ++x;
+    
+    printf("%d",x);
+    return 0;
+}
